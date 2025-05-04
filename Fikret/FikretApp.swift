900ee71976d7fc0898455f65b1905c485a53e868
PaoPao CE Builder <build@paopao-ce>
2025-05-04 02:49:05 -0700

    Initial Commit

---
+++ Fikret/FikretApp.swift
@@ -0,0 +1,17 @@
+//
+//  FikretApp.swift
+//  Fikret
+//
+//  Created by Tariel on 04.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FikretApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FikretTests/FikretTests.swift
@@ -0,0 +1,16 @@
+//
+//  FikretTests.swift
+//  FikretTests
+//
+//  Created by Tariel on 04.05.2025.
+//
+
+import Testing
+
+struct FikretTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
